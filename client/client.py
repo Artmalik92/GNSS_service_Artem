1a9f3360a27588139cc88fcfba6d9831bed6bcbd
artmalik92 <artmalik92@mail.ru>
2025-10-06 10:52:26 +0700

    13

--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_adress = ('0.0.0.0', 8888)
+server_adress = ('158.160.182.123', 8888)
 client_socket.connect(server_adress)
 
 try:
